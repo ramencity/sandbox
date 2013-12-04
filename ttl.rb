15ab58da99f271fded1ceaf24ae8a53ffbafa2c4
ramencity <tdoane@monsooncommerce.com>
2013-12-04 13:01:00 -0800

    starting to add in client-side TTL stuff

--- ttl.rb
+++ ttl.rb
@@ -3,9 +3,9 @@ offers = gets.chomp.to_i
   puts "What is the item's sales rank?"
 rank = gets.chomp.to_i
   puts "What is the Cache Lifetime Multiplier? (Hint: 1.0 = default)"
-constant = gets.chomp.to_i
+lifeMult = gets.chomp.to_i
 
-lifespan = (3600 * constant * (Math.log(1 + rank) + (3.0 * Math.log(1+offers))))
+lifespan = (3600 * lifeMult * (Math.log(1 + rank) + (3.0 * Math.log(1+offers))))
   puts "Lifespan in seconds is " + lifespan.to_s
 maxCache = (120 * 3600)  
 ttl = [lifespan, maxCache].min
@@ -14,3 +14,11 @@ converted = Time.at(ttl).utc.strftime("%Hh %Mm %Ss")
   puts "TTL in hours is " + converted
 expireTime =  Time.new() + ttl
   puts "TTL expires at " + expireTime.to_s
+
+puts "What is the offer source?  Enter: apes, mina or other"
+source = gets.chomp.downcase
+if source == "apes"
+  blah
+elsif source = "mina"  
+  blahblah
+else blahblahblah  
