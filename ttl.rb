6e24bb2820640fabbae70caf67e2200ef7c76da4
ramencity <tdoane@monsooncommerce.com>
2013-12-04 12:48:42 -0800

    added in MaxCache (currently hard-coded to 120 hours from config)

--- ttl.rb
+++ ttl.rb
@@ -1,12 +1,16 @@
-puts "What is the count of competitive offers?"
+  puts "What is the count of competitive offers?"
 offers = gets.chomp.to_i
-puts "What is the item's sales rank?"
+  puts "What is the item's sales rank?"
 rank = gets.chomp.to_i
-puts "What is the Cache Lifetime Multiplier? (Hint: 1.0 = default)"
+  puts "What is the Cache Lifetime Multiplier? (Hint: 1.0 = default)"
 constant = gets.chomp.to_i
-ttl = (3600 * constant * (Math.log(1 + rank)  + (3.0 * Math.log(1+offers))))
-puts "TTL in seconds is " + ttl.to_s
+
+lifespan = (3600 * constant * (Math.log(1 + rank) + (3.0 * Math.log(1+offers))))
+  puts "Lifespan in seconds is " + lifespan.to_s
+maxCache = (120 * 3600)  
+ttl = [lifespan, maxCache].min
+
 converted = Time.at(ttl).utc.strftime("%Hh %Mm %Ss")
-puts "TTL in hours is " + converted
-cached = Time.new() + ttl
-puts "Cache expires at " + cached.to_s
+  puts "TTL in hours is " + converted
+expireTime =  Time.new() + ttl
+  puts "TTL expires at " + expireTime.to_s
