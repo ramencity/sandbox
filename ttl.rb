86211d0fe78a3914eb0888da8466cced2f19f0df
ramencity <tdoane@monsooncommerce.com>
2013-12-04 15:21:51 -0800

    adding in source multipliers, fallback recovery

--- ttl.rb
+++ ttl.rb
@@ -1,3 +1,4 @@
+acquiredAt = Time.new()
   puts "What is the count of competitive offers?"
 offers = gets.chomp.to_i
   puts "What is the item's sales rank?"
@@ -10,15 +11,33 @@ lifespan = (3600 * lifeMult * (Math.log(1 + rank) + (3.0 * Math.log(1+offers))))
 maxCache = (120 * 3600)  
 ttl = [lifespan, maxCache].min
 
-converted = Time.at(ttl).utc.strftime("%Hh %Mm %Ss")
-  puts "TTL in hours is " + converted
-expireTime =  Time.new() + ttl
+convertTtl = Time.at(ttl).utc.strftime("%Hh %Mm %Ss")
+  puts "TTL in hours is " + convertTtl
+expireTime =  acquiredAt + ttl
   puts "TTL expires at " + expireTime.to_s
 
 puts "What is the offer source?  Enter: apes, mina or other"
 source = gets.chomp.downcase
 if source == "apes"
-  blah
-elsif source = "mina"  
-  blahblah
-else blahblahblah  
+  @multiplier = 0.5
+elsif source == "mina"  
+  @multiplier = 0.5
+else @multiplier = 1.0
+end  
+
+expireHours = ttl * @multiplier
+expireConvert =  Time.at(expireHours).utc.strftime("%Hh %Mm %Ss")
+  puts "expiration after considering multipliers is " + expireConvert
+expireTime = acquiredAt + expireHours  
+  
+recovery = Time.new() + (6*3600)
+  puts "recovery time is " + recovery.to_s
+  puts "Were these offers obtained via fallback mechanism?  Answer: y or n"
+isFallback = gets.chomp.downcase
+if isFallback == "y"
+  nuExpiry = [expireTime.to_i, recovery.to_i].max
+elsif isFallback == "n"
+  nuExpiry = expireTime
+end 
+
+puts "New expiry after considering fallback case is " + Time.at(nuExpiry).to_s
