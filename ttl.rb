12b71dc8f34a89d7c1563f5208fd0eea29715397
ramencity <tdoane@monsooncommerce.com>
2013-12-04 16:01:01 -0800

    renaming things to make clearer; abstracting more variables

--- ttl.rb
+++ ttl.rb
@@ -1,5 +1,4 @@
 acquiredAt = (Time.new() - (24*3600))
-  puts "Item was acquired at " + Time.at(acquiredAt).to_s
   puts "What is the count of competitive offers?"
 offers = gets.chomp.to_i
   puts "What is the item's sales rank?"
@@ -7,15 +6,16 @@ rank = gets.chomp.to_i
   puts "What is the Cache Lifetime Multiplier? (Hint: 1.0 = default)"
 lifeMult = gets.chomp.to_i
 
-lifespan = (3600 * lifeMult * (Math.log(1 + rank) + (3.0 * Math.log(1+offers))))
-  puts "Lifespan in seconds is " + lifespan.to_s
-maxCache = (120 * 3600)  
-ttl = [lifespan, maxCache].min
+lifespan = (3600 * lifeMult * (Math.log(1 + rank) + (3.0 * Math.log(1 + offers))))
+#  puts "Lifespan in seconds is " + lifespan.to_s
+maxCacheLifetimeInHours = 120
+maxCacheCap = (maxCacheLifetimeInHours * 3600)  
+ttl = [lifespan, maxCacheCap].min
 
 convertTtl = Time.at(ttl).utc.strftime("%Hh %Mm %Ss")
   puts "TTL in hours is " + convertTtl
 expireTime =  acquiredAt + ttl
-  puts "TTL would expire at " + expireTime.to_s
+  puts "Based on acquired time of " + Time.at(acquiredAt).to_s + ", TTL would expire at " + expireTime.to_s
 
 puts "What is the offer source?  Enter: apes, mina or other"
 source = gets.chomp.downcase
@@ -28,22 +28,22 @@ end
 
 expireHours = ttl * @multiplier
 expireConvert =  Time.at(expireHours).utc.strftime("%Hh %Mm %Ss")
-  puts "expiration hours after considering multipliers is " + expireConvert
-expireTime = acquiredAt + expireHours  
-  
+  puts "TTL in hours after considering source is " + expireConvert
+expireAt = acquiredAt + expireHours
+
 recoveryHours = 6
 recovery = Time.new() + (recoveryHours * 3600)
-  puts "recovery time from fallback scenario would be " + recovery.to_s
+  puts "Recovery time from fallback scenario would be " + recovery.to_s
   puts "Were these offers obtained via fallback mechanism?  Answer: y or n"
 isFallback = gets.chomp.downcase
 if isFallback == "y"
-  nuExpiry = [expireTime.to_i, recovery.to_i].max
+  newExpiry = [expireAt.to_i, recovery.to_i].max
 elsif isFallback == "n"
-  nuExpiry = expireTime
+  newExpiry = expireAt
 end 
 
-puts "New expiry after considering fallback case is " + Time.at(nuExpiry).to_s
+puts "Expiry after considering fallback case is " + Time.at(newExpiry).to_s
 
 minTTLHours = 2
-cacheUntil = [nuExpiry.to_i, (Time.new() + (minTTLHours * 3600)).to_i].max
-  puts "Final client cache after considering acquired time vs minimum TTL is: " + Time.at(cacheUntil).to_s
+cacheUntil = [newExpiry.to_i, (Time.new() + (minTTLHours * 3600)).to_i].max
+  puts "Final client cache expiry after considering acquired time vs minimum TTL is: " + Time.at(cacheUntil).to_s
