79921c16915f7008c1950f31e7f6b606a8178284
ramencity <tdoane@monsooncommerce.com>
2014-04-14 14:52:25 -0700

    v 0.0.0.2 - one file: asks if you want to encode or decode

--- fileconverter/file_converter.rb
+++ fileconverter/file_converter.rb
@@ -8,15 +8,30 @@ rawfile = gets.chomp
 readme = File.open(".//" + rawfile)
 readit = File.read(readme)
 
-#gzip the file:
-zipup = StringIO.new("w")
-gzip = Zlib::GzipWriter.new(zipup)
-gzip.write(readit)
-gzip.close
-zipped = zipup.string
+puts "do you want to encode or decode the file?"
+action = gets.chomp.downcase
 
-#then base64 encode the puppy:
-encoded = Base64.encode64(zipped).force_encoding('UTF-8')
-File.open(".//" + rawfile + "_encoded.gz", "w") {|file| file.puts encoded}
+  if action == 'encode'
+    #gzip the file:
+    zipup = StringIO.new("w")
+    gzip = Zlib::GzipWriter.new(zipup)
+    gzip.write(readit)
+    gzip.close
+    zipped = zipup.string
 
-puts "Your new file is called " + rawfile + "_encoded.gz"
+    #then base64 encode the puppy:
+    encoded = Base64.encode64(zipped).force_encoding('UTF-8')
+    File.open(".//" + rawfile + "_encoded.gz", "w") {|file| file.puts encoded}
+
+    puts "Your new file is called " + rawfile + "_encoded.gz"
+
+  elsif action == 'decode'
+    #first we base64 decode the puppy:
+    decoded = Base64.decode64(readit)
+
+    #then we g-unzip the file:
+    unzip = Zlib::GzipReader.new(StringIO.new(decoded))
+    File.open(".//" + rawfile + "_decoded.json", "w") {|file| file.puts unzip.read}
+
+    puts "Your new file is called " + rawfile + "_decoded.json"
+end
