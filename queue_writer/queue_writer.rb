6541201da0a75cd9399d8d74d469841cb578f607
ramencity <tdoane@monsooncommerce.com>
2014-04-22 16:39:38 -0700

    removed the weird message numbering stuff that came from the stomp-write sample, and added .strip to gets calls

--- queue_writer/queue_writer.rb
+++ queue_writer/queue_writer.rb
@@ -6,22 +6,22 @@ require 'stomp'
 
 begin
   puts 'What is the name of the queue you want to write to?'
-  queue = gets.chomp
+  queue = gets.chomp.strip
   @queue_name = "/queue/" + queue
 
   puts 'What file in your current directory do you want to send to the queue?'
-  rawfile = gets.chomp
-    while File.exist?(".//" + rawfile) == false
-      puts "I don't see that file in here! Try again"
-      rawfile = gets.chomp
-    end
+  rawfile = gets.chomp.strip
+  while File.exist?(".//" + rawfile) == false
+    puts "I don't see that file in here! Try again"
+    rawfile = gets.chomp.strip
+  end
 
-  puts 'Do you want your message zipped up and base-64 encoded?  Answer yes or no'
-  message_encoded = gets.chomp.downcase
-    until message_encoded == 'yes' || message_encoded == 'no'
-      puts "that is not an option! Enter yes or no"
-      message_encoded = gets.chomp
-    end
+  puts 'Do you want your message zipped up and base-64 encoded? Answer yes or no'
+  message_encoded = gets.chomp.strip.downcase
+  until message_encoded == 'yes' || message_encoded == 'no'
+    puts "that is not an option! Enter yes or no"
+    message_encoded = gets.chomp.strip.downcase
+  end
 
   if message_encoded == 'yes'
     readfile = File.read(rawfile)
@@ -30,23 +30,15 @@ begin
     gzip.write(readfile)
     gzip.close
     zipstring = zipup.string
-    sendfile = Base64.encode64(zipstring).force_encoding('UTF-8')
+    message = Base64.encode64(zipstring).force_encoding('UTF-8')
   else
-    sendfile = File.read(rawfile)
+    message = File.read(rawfile)
   end
 
-  #File.open(".//sendfile.json", "w") {|f| f.puts sendfile}
-
-  puts 'Ok, attempting to connect to queue ' + queue
-  #below stolen from https://github.com/stompgem/stomp/blob/dev/examples/publisher.rb
-
-       client = Stomp::Client.new('admin', 'password', "backend.mss.monsoonqa.com", 61613)
-       message = sendfile
-
-       for i in (1..2)
-         puts "Sending message"
-         client.publish(@queue_name, "#{i}: #{message}", {:persistent => true})
-         puts "(#{Time.now}) Message sent: #{i}"
-         sleep 0.2
-       end
+    puts 'Ok, attempting to connect to queue ' + queue
+  client = Stomp::Client.new('admin', 'password', "backend.mss.monsoonqa.com", 61613)
+    puts 'Sending message'
+  client.publish(@queue_name, "#{message}", {:persistent => true})
+    puts "(#{Time.now}) Message sent - cheers!"
+   
 end
